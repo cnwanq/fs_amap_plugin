7c0e785b6ab108c8438b0ac991783159028c7c0f
wanqijian <wanqijian@icloud.com>
2019-04-10 11:59:39 +0800

    first init

---
+++ ios/Classes/FsAmapPlugin.m
@@ -0,0 +1,20 @@
+#import "FsAmapPlugin.h"
+
+@implementation FsAmapPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"fs_amap_plugin"
+            binaryMessenger:[registrar messenger]];
+  FsAmapPlugin* instance = [[FsAmapPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
